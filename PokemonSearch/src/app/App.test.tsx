15d0091d6f80588fd74dd4a77aad51b296e332c1
Jazzlyn <jazzlyn@Jazzlyns-MacBook-Pro.local>
2025-11-12 13:13:51 -0500

    add vitest

--- PokemonSearch/src/app/App.test.tsx
+++ PokemonSearch/src/app/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { getByText, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders the App component', () => {
+    render(<App />);
+
+    const headingElement = screen.getByText('Pokémon');
+    expect(headingElement).toBeDefined();
+  });
 });
